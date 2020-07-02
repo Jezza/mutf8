286b9774a83834b975e67e7965a3f21e826c2b51
Jezza <jezzadabomb@gmail.com>
2020-07-02 17:40:19 +0200

    Add serde support.
    Fixes #1

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,15 @@ use std::fmt::{
 };
 use std::ops::Deref;
 
+mod conversion;
+
+pub use conversion::mutf8_to_utf8;
+pub use conversion::utf8_to_mutf8;
+
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize, Serializer, Deserializer};
+use serde::de::SeqAccess;
+
 #[derive(Eq, PartialEq, Hash, Clone)]
 pub struct MString {
 	inner: Box<[u8]>
@@ -122,6 +131,45 @@ impl MString {
 
 	pub fn as_utf8_bytes(&self) -> Cow<[u8]> {
 		mutf8_to_utf8(&self.inner)
+	}
+}
+
+
+#[cfg(feature = "serde")]
+impl Serialize for MString {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+		serializer.serialize_bytes(&self.inner)
+	}
+}
+
+#[cfg(feature = "serde")]
+struct MStringVisitor;
+
+#[cfg(feature = "serde")]
+impl<'de> serde::de::Visitor<'de> for MStringVisitor {
+	type Value = MString;
+
+	fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+		formatter.write_str("mutf8 bytes")
+	}
+
+	fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error> where A: SeqAccess<'de>, {
+		let mut data = vec![];
+		while let Some(val) = seq.next_element::<u8>()? {
+			data.push(val);
+		}
+		Ok(MString::from_mutf8(data))
+	}
+
+	fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E> where E: serde::de::Error {
+		Ok(MString::from_mutf8(v))
+	}
+}
+
+#[cfg(feature = "serde")]
+impl<'de> Deserialize<'de> for MString {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+		deserializer.deserialize_bytes(MStringVisitor)
 	}
 }
 
@@ -339,201 +387,5 @@ impl AsRef<mstr> for MString {
 impl Debug for mstr {
 	fn fmt(&self, f: &mut Formatter) -> FResult {
 		Debug::fmt(self.to_utf8().as_ref(), f)
-	}
-}
-
-fn utf8_to_mutf8(input: &[u8]) -> Cow<[u8]> {
-	let len = input.len();
-	if len == 0 {
-		return Cow::Borrowed(input);
-	}
-
-	const MODE_BORROW: u8 = 0;
-	const MODE_COPY: u8 = 1;
-
-	let mut mode = MODE_BORROW;
-
-	let mut data = vec![];
-	let mut i = 0;
-	while i < len {
-		let mark = i;
-
-		let byte1 = unsafe { *input.get_unchecked(i) };
-		i += 1;
-
-		// nul bytes and bytes starting with 11110xxx are somewhat special
-		if byte1 & 0x80 == 0 { // 1-byte encoding
-			if byte1 == 0 {
-				if mode == MODE_BORROW {
-					mode = MODE_COPY;
-					let run = &input[0..mark];
-					data.extend(run);
-				}
-				data.push(0xC0);
-				data.push(0x80);
-			} else if mode == MODE_COPY {
-				data.push(byte1);
-			}
-		} else if byte1 & 0xE0 == 0xC0 { // 2-byte encoding
-			if mode == MODE_COPY {
-				data.push(byte1);
-				let byte2 = *input.get(i).unwrap_or(&0);
-				i += 1;
-				data.push(byte2);
-			}
-		} else if byte1 & 0xF0 == 0xE0 { // 3-byte encoding
-			if mode == MODE_COPY {
-				data.push(byte1);
-				let byte2 = *input.get(i).unwrap_or(&0);
-				i += 1;
-				data.push(byte2);
-				let byte3 = *input.get(i).unwrap_or(&0);
-				i += 1;
-				data.push(byte3);
-			}
-		} else if byte1 & 0xF8 == 0xF0 {
-			if mode == MODE_BORROW {
-				mode = MODE_COPY;
-				let run = &input[0..mark];
-				data.extend(run);
-			}
-
-			// Beginning of 4-byte encoding, turn into 2 3-byte encodings
-			// Bits in: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-			let byte2 = *input.get(i).unwrap_or(&0);
-			i += 1;
-			let byte3 = *input.get(i).unwrap_or(&0);
-			i += 1;
-			let byte4 = *input.get(i).unwrap_or(&0);
-			i += 1;
-
-			// Reconstruct full 21-bit value
-			let mut bits: u32 = ((byte1 as u32) & 0x07) << 18;
-			bits += ((byte2 as u32) & 0x3F) << 12;
-			bits += ((byte3 as u32) & 0x3F) << 6;
-			bits += ((byte4 as u32) & 0x3F);
-
-			// Bits out: 11101101 1010xxxx 10xxxxxx
-			data.push(0xED);
-			data.push((0xA0 + (((bits >> 16) - 1) & 0x0F)) as u8);
-			data.push((0x80 + ((bits >> 10) & 0x3F)) as u8);
-
-			// Bits out: 11101101 1011xxxx 10xxxxxx
-			data.push(0xED);
-			data.push((0xB0 + ((bits >> 6) & 0x0F)) as u8);
-			data.push(byte4);
-		}
-	}
-
-	if mode == MODE_BORROW {
-		Cow::Borrowed(input)
-	} else {
-		Cow::Owned(data)
-	}
-}
-
-fn mutf8_to_utf8(input: &[u8]) -> Cow<[u8]> {
-	let len = input.len();
-	if len == 0 {
-		return Cow::Borrowed(input);
-	}
-
-	const MODE_BORROW: u8 = 0;
-	const MODE_COPY: u8 = 1;
-
-	let mut mode = MODE_BORROW;
-
-	let mut data = vec![];
-	let mut i = 0;
-	while i < len {
-		let mark = i;
-
-		let byte1 = unsafe { *input.get_unchecked(i) };
-		i += 1;
-
-		if byte1 & 0x80 == 0 { // 1 byte encoding
-			if mode == MODE_BORROW {
-				// Nothing to do here as it's valid ascii/utf-8.
-				continue;
-			}
-			data.push(byte1);
-		} else if byte1 & 0xE0 == 0xC0 { // 2 byte encoding
-			// Mask out the three bits so we can check if it's equal to the marker bits that say this is a 2 byte encoding.
-			// 0b11100000 = 0xE0
-			// 0b11000000 = 0xC0
-			let byte2 = *input.get(i).unwrap_or(&0);
-			i += 1;
-//				println!("Bytes: {:x} {:x}", byte1, byte2);
-
-			if byte1 != 0xC0 || byte2 != 0x80 {
-				if mode == MODE_BORROW {
-					// Nothing to do here as it's valid ascii/utf-8.
-					continue;
-				}
-				data.push(byte1);
-				data.push(byte2);
-			} else {
-				if mode == MODE_BORROW {
-					mode = MODE_COPY;
-					let run = &input[0..mark];
-					data.extend(run);
-				}
-				data.push(0);
-			}
-		} else if byte1 & 0xF0 == 0xE0 { // 3 byte encoding
-			let byte2 = *input.get(i).unwrap_or(&0);
-			i += 1;
-			let byte3 = *input.get(i).unwrap_or(&0);
-			i += 1;
-//				println!("{:x} {:x} {:x}", byte1, byte2, byte3);
-			if i + 2 < len && byte1 == 0xED && byte2 & 0xF0 == 0xA0 {
-				// Check if pair encoding...
-
-				let byte4 = *input.get(i).unwrap_or(&0);
-				let byte5 = *input.get(i + 1).unwrap_or(&0);
-				let byte6 = *input.get(i + 2).unwrap_or(&0);
-
-//					println!("{:x} {:x} {:x}", byte4, byte5, byte6);
-				if byte4 == 0xED && byte5 & 0xF0 == 0xB0 {
-					// Bits in: 11101101 1010xxxx 10xxxxxx
-					// Bits in: 11101101 1011xxxx 10xxxxxx
-
-					i += 2;
-
-					let mut bits: u32 = (((byte2 as u32) & 0x0F) + 1) << 16;
-					bits += ((byte3 as u32) & 0x3F) << 10;
-					bits += ((byte5 as u32) & 0x0F) << 6;
-					bits += (byte6 as u32) & 0x3F;
-
-					// Bits out: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-
-					if mode == MODE_BORROW {
-						mode = MODE_COPY;
-						let run = &input[0..mark];
-						data.extend(run);
-					}
-					// Convert the bits into 4 UTF-8 bytes.
-					data.push((0xF0 + ((bits >> 18) & 0x07)) as u8);
-					data.push((0x80 + ((bits >> 12) & 0x3F)) as u8);
-					data.push((0x80 + ((bits >> 6) & 0x3F)) as u8);
-					data.push((0x80 + (bits & 0x3F)) as u8);
-
-					continue;
-				}
-			}
-			if mode == MODE_BORROW {
-				// Nothing to do here as it's valid ascii/utf-8.
-				continue;
-			}
-			data.push(byte1);
-			data.push(byte2);
-			data.push(byte3);
-		}
-	}
-
-	if mode == MODE_BORROW {
-		Cow::Borrowed(input)
-	} else {
-		Cow::Owned(data)
 	}
 }
---
+++ tests/serde.rs
@@ -0,0 +1,46 @@
+
+
+use serde::{Serialize, Deserialize};
+use mutf8::{MString, utf8_to_mutf8};
+
+#[derive(Serialize, Deserialize)]
+struct Blah(u64, MString, String);
+
+#[test]
+fn test_serialise() {
+	let value = MString::from_utf8("Hello, World!");
+
+	let value = Blah(64, value, String::new());
+
+	let output = serde_json::to_string(&value).unwrap();
+	assert_eq!(output, r#"[64,[72,101,108,108,111,44,32,87,111,114,108,100,33],""]"#);
+}
+
+#[test]
+fn test_deserialise() {
+	let input = r#"[64,[72,101,108,108,111,44,32,87,111,114,108,100,33],""]"#;
+
+	let output: Blah = serde_json::from_str(&input).unwrap();
+
+	assert_eq!(output.1, MString::from_utf8("Hello, World!"));
+}
+
+#[test]
+fn test_serialise_nul() {
+	let value = MString::from_utf8("Hello, \0World!");
+
+	let value = Blah(64, value, String::new());
+
+	let output = serde_json::to_string(&value).unwrap();
+	assert_eq!(output, r#"[64,[72,101,108,108,111,44,32,192,128,87,111,114,108,100,33],""]"#);
+}
+
+#[test]
+fn test_deserialise_nul() {
+
+	let input = r#"[64,[72,101,108,108,111,44,32,192,128,87,111,114,108,100,33],""]"#;
+
+	let output: Blah = serde_json::from_str(&input).unwrap();
+
+	assert_eq!(output.1, MString::from_utf8("Hello, \0World!"));
+}
